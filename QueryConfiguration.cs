a962ee811d0b412aaa211a087d068e733b1172c3
Makesh Vineeth <makeshvineeth1997@gmail.com>
2021-05-19 21:39:46 +0530

    Improve modularity

---
+++ QueryConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WikiPreview.Fluent.Plugin
+{
+    class QueryConfiguration
+    {
+        public string SearchTerm { get; set; }
+        public int SentenceCount { get; set; }
+        public int WikiNameSpace { get; set; }
+        public int ResultsCount { get; set; }
+        public int ImageSize { get; set; }
+    }
+}
