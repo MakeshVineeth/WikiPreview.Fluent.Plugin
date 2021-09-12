0567963275255d42f682feeba3e0cb587c202e38
Makesh Vineeth <makeshvineeth1997@gmail.com>
2021-09-12 20:39:20 +0530

    Custom Preview - 4

--- WikiPreview.Fluent.Plugin/CustomPreview.cs
+++ WikiPreview.Fluent.Plugin/CustomPreview.cs
@@ -5,11 +5,15 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using Avalonia.Input;
+using Avalonia.Interactivity;
 using Avalonia.Layout;
 using Avalonia.Media;
 using Avalonia.Media.Imaging;
+using Blast.API.Core.Processes;
 using Blast.API.Graphics;
+using Blast.API.Processes;
 using Blast.Core.Interfaces;
+using TextCopy;
 using static WikiPreview.Fluent.Plugin.WikiPreviewSearchResult;
 using static System.Environment;
 
@@ -17,6 +21,10 @@ namespace WikiPreview.Fluent.Plugin
 {
     public class CustomPreview : IResultPreviewControlBuilder
     {
+        private const string GoogleStr = "Search Google";
+        private const string WikipediaStr = "Wikipedia";
+        private const string CopyStr = "Copy Text";
+
         public CustomPreview()
         {
             PreviewBuilderDescriptor = new PreviewBuilderDescriptor
@@ -36,15 +44,20 @@ namespace WikiPreview.Fluent.Plugin
 
         public ValueTask<Control> CreatePreviewControl(ISearchResult searchResult)
         {
-            Control control = GeneratePreview(searchResult.ResultName,
-                searchResult.PreviewImage.ConvertToAvaloniaBitmap());
+            if (searchResult is not WikiPreviewSearchResult wikiPreviewSearchResult)
+                throw new InvalidCastException(nameof(wikiPreviewSearchResult));
+
+            Control control = GeneratePreview(wikiPreviewSearchResult);
             return new ValueTask<Control>(control);
         }
 
         public PreviewBuilderDescriptor PreviewBuilderDescriptor { get; }
 
-        private static Control GeneratePreview(string text, IBitmap bitmap)
+        private static Control GeneratePreview(WikiPreviewSearchResult searchResult)
         {
+            string text = searchResult.ResultName;
+            Bitmap bitmap = searchResult.PreviewImage.ConvertToAvaloniaBitmap();
+
             // double the new lines for better reading.
             text = Regex.Replace(text, @"\r\n?|\n", NewLine + NewLine);
 
@@ -88,15 +101,15 @@ namespace WikiPreview.Fluent.Plugin
             // Create Parent Grid.
             var grid = new Grid
             {
-                Margin = new Thickness(10.0, 0, 10, 10),
+                Margin = new Thickness(10.0, 0, 10, 10)
             };
             grid.RowDefinitions.Add(new RowDefinition());
             grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
             grid.Children.Add(scrollViewer);
 
-            Button openWiki = CreateButton("Wikipedia");
-            Button searchGoogle = CreateButton("Search Google");
-            Button copyContents = CreateButton("Copy Text");
+            Button openWiki = CreateButton(WikipediaStr, searchResult.Url);
+            Button searchGoogle = CreateButton(GoogleStr, searchResult.DisplayedName);
+            Button copyContents = CreateButton(CopyStr, text);
 
             var uniformGrid = new UniformGrid
             {
@@ -127,15 +140,38 @@ namespace WikiPreview.Fluent.Plugin
                 scrollViewer.VerticalScrollBarVisibility = ScrollBarVisibility.Auto;
         }
 
-        private static Button CreateButton(string text)
+        private static Button CreateButton(string text, string tag)
         {
-            return new Button
+            var button = new Button
             {
                 Content = text,
                 Margin = new Thickness(0, 0, 5, 0),
                 HorizontalAlignment = HorizontalAlignment.Stretch,
-                VerticalAlignment = VerticalAlignment.Stretch
+                VerticalAlignment = VerticalAlignment.Stretch,
+                Tag = tag
             };
+
+            button.Click += ButtonOnClick;
+            return button;
+        }
+
+        private static void ButtonOnClick(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            string buttonContent = button?.Content.ToString();
+
+            if (string.IsNullOrWhiteSpace(buttonContent)) return;
+
+            string buttonTag = button.Tag?.ToString();
+
+            if (string.IsNullOrWhiteSpace(buttonTag)) return;
+
+            IProcessManager managerInstance = ProcessUtils.GetManagerInstance();
+            if (buttonContent.Contains(WikipediaStr))
+                managerInstance.StartNewProcess(WikiRootUrl + buttonTag);
+            else if (buttonContent.Contains(GoogleStr))
+                managerInstance.StartNewProcess(GoogleSearchUrl + buttonTag);
+            else if (buttonContent.Contains(CopyStr)) Clipboard.SetText(buttonTag);
         }
     }
 }
