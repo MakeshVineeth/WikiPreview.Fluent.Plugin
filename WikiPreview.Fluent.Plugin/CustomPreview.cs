52ca12a8a3c2457f2f71b4dcadfe4450b5636f24
Makesh Vineeth <makeshvineeth1997@gmail.com>
2021-09-12 18:25:24 +0530

    Custom Preview - 3

--- WikiPreview.Fluent.Plugin/CustomPreview.cs
+++ WikiPreview.Fluent.Plugin/CustomPreview.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using Windows.UI.ViewManagement;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
+using Avalonia.Input;
+using Avalonia.Layout;
 using Avalonia.Media;
 using Avalonia.Media.Imaging;
 using Blast.API.Graphics;
@@ -35,42 +36,22 @@ namespace WikiPreview.Fluent.Plugin
 
         public ValueTask<Control> CreatePreviewControl(ISearchResult searchResult)
         {
-            Control control = GeneratePreview(searchResult.DisplayedName, searchResult.ResultName,
+            Control control = GeneratePreview(searchResult.ResultName,
                 searchResult.PreviewImage.ConvertToAvaloniaBitmap());
             return new ValueTask<Control>(control);
         }
 
         public PreviewBuilderDescriptor PreviewBuilderDescriptor { get; }
 
-        private static Control GeneratePreview(string title, string text, IBitmap bitmap)
+        private static Control GeneratePreview(string text, IBitmap bitmap)
         {
             // double the new lines for better reading.
             text = Regex.Replace(text, @"\r\n?|\n", NewLine + NewLine);
 
-            // creates heading.
-            var header = new TextBlock
-            {
-                Text = title,
-                FontWeight = FontWeight.SemiBold,
-                FontSize = 20.0
-            };
-
-            // creates separator
-            var defaultTheme = new UISettings();
-            string uiTheme = defaultTheme.GetColorValue(UIColorType.Foreground).ToString();
-            Color lineColor = Color.Parse(uiTheme);
-
-            var separator = new Border
-            {
-                BorderThickness = new Thickness(0.6),
-                BorderBrush = new SolidColorBrush(lineColor),
-                Margin = new Thickness(0, 8)
-            };
-
             // creates article content.
             var wikiDescription = new TextBlock
             {
-                Text = text, Padding = new Thickness(0, 10, 0, 0), TextWrapping = TextWrapping.Wrap,
+                Text = text, Padding = new Thickness(5, 10, 5, 0), TextWrapping = TextWrapping.Wrap,
                 TextTrimming = TextTrimming.WordEllipsis
             };
 
@@ -89,20 +70,72 @@ namespace WikiPreview.Fluent.Plugin
                 MaxWidth = FixedImageSize
             };
 
+            // StackPanel to store image and text.
             var stackPanel = new StackPanel();
-            stackPanel.Children.Add(header);
-            stackPanel.Children.Add(separator);
             stackPanel.Children.Add(imageControl);
             stackPanel.Children.Add(wikiDescription);
 
             var scrollViewer = new ScrollViewer
             {
                 Content = stackPanel,
-                Margin = new Thickness(10.0, 0),
-                VerticalScrollBarVisibility = ScrollBarVisibility.Hidden
+                VerticalScrollBarVisibility = ScrollBarVisibility.Hidden,
+                Margin = new Thickness(0, 0, 0, 5)
             };
 
-            return scrollViewer;
+            scrollViewer.PointerEnter += ScrollViewerOnPointerEnter;
+            scrollViewer.PointerLeave += ScrollViewerOnPointerLeave;
+
+            // Create Parent Grid.
+            var grid = new Grid
+            {
+                Margin = new Thickness(10.0, 0, 10, 10),
+            };
+            grid.RowDefinitions.Add(new RowDefinition());
+            grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+            grid.Children.Add(scrollViewer);
+
+            Button openWiki = CreateButton("Wikipedia");
+            Button searchGoogle = CreateButton("Search Google");
+            Button copyContents = CreateButton("Copy Text");
+
+            var uniformGrid = new UniformGrid
+            {
+                Columns = 3,
+                Rows = 0
+            };
+
+            uniformGrid.Children.Add(openWiki);
+            uniformGrid.Children.Add(searchGoogle);
+            uniformGrid.Children.Add(copyContents);
+
+            // add buttons grid to the bottom row.
+            Grid.SetRow(uniformGrid, 1);
+            grid.Children.Add(uniformGrid);
+
+            return grid;
+        }
+
+        private static void ScrollViewerOnPointerLeave(object sender, PointerEventArgs e)
+        {
+            if (sender is ScrollViewer scrollViewer)
+                scrollViewer.VerticalScrollBarVisibility = ScrollBarVisibility.Hidden;
+        }
+
+        private static void ScrollViewerOnPointerEnter(object sender, PointerEventArgs e)
+        {
+            if (sender is ScrollViewer scrollViewer)
+                scrollViewer.VerticalScrollBarVisibility = ScrollBarVisibility.Auto;
+        }
+
+        private static Button CreateButton(string text)
+        {
+            return new Button
+            {
+                Content = text,
+                Margin = new Thickness(0, 0, 5, 0),
+                HorizontalAlignment = HorizontalAlignment.Stretch,
+                VerticalAlignment = VerticalAlignment.Stretch
+            };
         }
     }
 }
