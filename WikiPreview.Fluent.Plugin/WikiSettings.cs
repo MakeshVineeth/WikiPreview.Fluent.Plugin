6e34588befd89183719dc8144c006dea3d9aa43c
Makesh Vineeth <makeshvineeth1997@gmail.com>
2021-10-02 15:24:30 +0530

    Give default property values

--- WikiPreview.Fluent.Plugin/WikiSettings.cs
+++ WikiPreview.Fluent.Plugin/WikiSettings.cs
@@ -13,16 +13,16 @@ namespace WikiPreview.Fluent.Plugin
         [Setting(Name = "Max Results", Description = "Control number of results from Wikipedia", MinValue = 1,
             MaxValue = 30,
             DefaultValue = 8, IconGlyph = SearchResultIcon)]
-        public int MaxResults { get; set; }
+        public int MaxResults { get; set; } = 8;
 
         [Setting(Name = "Load Images", Description = "Whether to retrieve and load images from Wikipedia",
             DefaultValue = true, IconGlyph = "\uEB9F")]
-        public bool LoadImages { get; set; }
+        public bool LoadImages { get; set; } = true;
 
         [Setting(Name = "Max Image Size",
             Description = "Sets the max image size shown in the Preview window",
             DefaultValue = 150, IconGlyph = "\uE91B", MinValue = 80, MaxValue = 300, RequireRestart = true,
             IsAdvanced = true)]
-        public int ImageSize { get; set; }
+        public int ImageSize { get; set; } = 150;
     }
 }
